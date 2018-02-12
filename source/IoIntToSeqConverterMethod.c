d8f5169126ae75e3fb7eed8971299963e8f613e7
Ales Tsurko <ales.tsurko@gmail.com>
2018-02-12 19:31:57 +0300

    fix indent

--- source/IoIntToSeqConverterMethod.c
+++ source/IoIntToSeqConverterMethod.c
@@ -11,45 +11,45 @@
 static const char *protoId = "IntToSeqConverterMethod";
 
 IoTag *IoIntToSeqConverterMethod_newTag(void *state) {
-	IoTag *tag = IoTag_newWithName_(protoId);
+    IoTag *tag = IoTag_newWithName_(protoId);
 
-	IoTag_state_(tag, state);
+    IoTag_state_(tag, state);
 
-	IoTag_freeFunc_(tag, (IoTagFreeFunc *)IoIntToSeqConverterMethod_free);
+    IoTag_freeFunc_(tag, (IoTagFreeFunc *)IoIntToSeqConverterMethod_free);
 
-	IoTag_cloneFunc_(tag, (IoTagCloneFunc *)IoIntToSeqConverterMethod_rawClone);
+    IoTag_cloneFunc_(tag, (IoTagCloneFunc *)IoIntToSeqConverterMethod_rawClone);
 
-	return tag;
+    return tag;
 }
 
 IoIntToSeqConverterMethod *IoIntToSeqConverterMethod_proto(void *state) {
-	IoObject *self = IoObject_new(state);
-	IoObject_tag_(self, IoIntToSeqConverterMethod_newTag(state));
-	
-	IoState_registerProtoWithFunc_(state, self, IoIntToSeqConverterMethod_proto);
-	
-	{
-		IoMethodTable methodTable[] = {
-		{"convert", IoIntToSeqConverterMethod_convert},
-		{NULL, NULL},
-		};
-		
-		IoObject_addMethodTable_(self, methodTable);
-	}
-    
+    IoObject *self = IoObject_new(state);
+    IoObject_tag_(self, IoIntToSeqConverterMethod_newTag(state));
+
+    IoState_registerProtoWithFunc_(state, self, IoIntToSeqConverterMethod_proto);
+
+    {
+        IoMethodTable methodTable[] = {
+            {"convert", IoIntToSeqConverterMethod_convert},
+            {NULL, NULL},
+        };
+
+        IoObject_addMethodTable_(self, methodTable);
+    }
+
     IoObject_setSlot_to_(self, IOSYMBOL("inputTypeTag"), IOSYMBOL("i"));
 
     return self;
 }
 
 IoIntToSeqConverterMethod *IoIntToSeqConverterMethod_rawClone(IoIntToSeqConverterMethod *proto) {
-	IoIntToSeqConverterMethod *self = IoObject_rawClonePrimitive(proto);
-	return self;
+    IoIntToSeqConverterMethod *self = IoObject_rawClonePrimitive(proto);
+    return self;
 }
 
 IoIntToSeqConverterMethod *IoIntToSeqConverterMethod_new(void *state) {
-	IoObject *proto = IoState_protoWithId_(state, protoId);
-	return IOCLONE(proto);
+    IoObject *proto = IoState_protoWithId_(state, protoId);
+    return IOCLONE(proto);
 }
 
 void IoIntToSeqConverterMethod_free(IoIntToSeqConverterMethod *self) {}
@@ -59,5 +59,5 @@ IoObject *IoIntToSeqConverterMethod_convert(IoIntToSeqConverterMethod *self, IoO
     int value = IoMessage_locals_intArgAt_(m, locals, 0);
     char bytes[4];
     memcpy(bytes, &value, 4);
-	return IoSeq_newWithCString_(IOSTATE, bytes);
+    return IoSeq_newWithCString_(IOSTATE, bytes);
 }
